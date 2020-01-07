98d2bfd72ce5afd6ce335e3a5cb74ab6d2343b1e
Paweł Madej <pawel.madej@amukana.pl>
2020-01-07 15:31:51 +0100

    Initial Commit

---
+++ ErgoStack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ErgoStack
+//
+//  Created by Pawel Madej on 07/01/2020.
+//  Copyright © 2020 Pawel Madej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
