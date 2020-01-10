089b2664269304b675b0bf8cfc63b27d6d60e959
Paweł Madej <pawel.madej@amukana.pl>
2020-01-10 22:30:46 +0100

    Remove example API responses

--- ErgoStack/Models/Question.swift
+++ ErgoStack/Models/Question.swift
@@ -6,41 +6,6 @@
 //  Copyright © 2020 Pawel Madej. All rights reserved.
 //
 
-/* Example Question object
- {
-   "items": [
-     {
-       "tags": [
-         "r",
-         "date",
-         "lubridate"
-       ],
-       "owner": {
-         "reputation": 11,
-         "user_id": 12662447,
-         "user_type": "registered",
-         "profile_image": "https://www.gravatar.com/avatar/3a038d23db6ada803040cad3f8067901?s=128&d=identicon&r=PG&f=1",
-         "display_name": "ginopono",
-         "link": "https://stackoverflow.com/users/12662447/ginopono"
-       },
-       "is_answered": true,
-       "view_count": 34,
-       "answer_count": 5,
-       "score": 1,
-       "last_activity_date": 1578412550,
-       "creation_date": 1578407572,
-       "last_edit_date": 1578410487,
-       "question_id": 59630491,
-       "link": "https://stackoverflow.com/questions/59630491/r-converting-consecutive-dates-from-a-single-column-into-a-2-column-range",
-       "title": "R: Converting consecutive dates from a single column into a 2-column range"
-     }
-   ],
-   "has_more": true,
-   "quota_max": 10000,
-   "quota_remaining": 9946
- }
- */
-
 import Foundation
 
 struct Question: Decodable {
--- ErgoStack/Models/User.swift
+++ ErgoStack/Models/User.swift
@@ -6,50 +6,6 @@
 //  Copyright © 2020 Pawel Madej. All rights reserved.
 //
 
-/*
- {
-   "items": [
-     {
-       "badge_counts": {
-         "bronze": 33,
-         "silver": 14,
-         "gold": 0
-       },
-       "account_id": 1345039,
-       "is_employee": false,
-       "last_modified_date": 1573682649,
-       "last_access_date": 1578412941,
-       "reputation_change_year": 0,
-       "reputation_change_quarter": 0,
-       "reputation_change_month": 0,
-       "reputation_change_week": 0,
-       "reputation_change_day": 0,
-       "reputation": 701,
-       "creation_date": 1332421095,
-       "user_type": "registered",
-       "user_id": 1285959,
-       "accept_rate": 75,
-       "location": "Polska",
-       "website_url": "https://www.pawelmadej.com",
-       "link": "https://stackoverflow.com/users/1285959/pawe%c5%82-madej",
-       "profile_image": "https://i.stack.imgur.com/vWIwN.jpg?s=128&g=1",
-       "display_name": "Paweł Madej"
-     }
-   ],
-   "has_more": false,
-   "quota_max": 10000,
-   "quota_remaining": 9930
- }
-
- Fields from question:
- "reputation": 11,
- "user_id": 12662447,
- "user_type": "registered",
- "profile_image": "https://www.gravatar.com/avatar/3a038d23db6ada803040cad3f8067901?s=128&d=identicon&r=PG&f=1",
- "display_name": "ginopono",
- "link": "https://stackoverflow.com/users/12662447/ginopono"
-
- */
 import Foundation
 
 struct User: Decodable {
